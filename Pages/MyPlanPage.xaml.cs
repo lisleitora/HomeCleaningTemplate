f438f54b80e26aabc4027f8a0664204dcfb785a9
Lis <lisleitora@gmail.com>
2024-02-19 16:40:02 +0000

    MyPlanPage selec. celaning

--- Pages/MyPlanPage.xaml.cs
+++ Pages/MyPlanPage.xaml.cs
@@ -6,4 +6,33 @@ public partial class MyPlanPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void Button_Clicked(object sender, EventArgs e)
+    { bool click = PgPink2.IsVisible;
+        if (click == true)
+       { 
+        PgPink2.IsVisible = false;
+            click = false;
+        }
+        else
+        {
+            PgPink2.IsVisible = true;
+            click = true;
+        }
+    }
+
+    private void Button_Clicked2(object sender, EventArgs e)
+    {
+        bool click = PgPink1.IsVisible;
+        if (click == true)
+        {
+            PgPink1.IsVisible = false;
+            click = false;
+        }
+        else
+        {
+            PgPink1.IsVisible = true;
+            click = true;
+        }
+    }
 }
