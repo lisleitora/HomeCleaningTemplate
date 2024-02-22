108601b3428803b83d4c5a47e78e3d8736fd58b8
lisleitora <lisleitora@gmail.com>
2024-02-22 15:55:52 +0000

    Frequency buttons

--- Pages/MyPlanPage.xaml.cs
+++ Pages/MyPlanPage.xaml.cs
@@ -35,4 +35,54 @@ public partial class MyPlanPage : ContentPage
             click = true;
         }
     }
+
+    private void Weekly(object sender, EventArgs e)
+    {
+        bool clicked =! pgWeeklyOn.IsVisible;
+        if (clicked == true)
+        {
+            pgWeeklyOn.IsVisible = true;
+            pgMonthlyOn.IsVisible = false;
+            pgBiWeeklyOn.IsVisible = false;
+            clicked = false;
+        }
+        else
+        {
+            pgWeeklyOn.IsVisible = false;
+            clicked = true;
+        }
+    }
+    private void BiWeekly(object sender, EventArgs e)
+    {
+        bool clicked = !pgBiWeeklyOn.IsVisible;
+        if (clicked == true)
+        {
+            pgWeeklyOn.IsVisible = false;
+            pgMonthlyOn.IsVisible = false;
+            pgBiWeeklyOn.IsVisible = true;
+            clicked = false;
+        }
+        else
+        {
+            pgBiWeeklyOn.IsVisible = false;
+            clicked = true;
+        }
+    }
+    private void Monthly(object sender, EventArgs e)
+    {
+
+        bool clicked = !pgMonthlyOn.IsVisible;
+        if (clicked == true)
+        {
+            pgWeeklyOn.IsVisible = false;
+            pgMonthlyOn.IsVisible = true;
+            pgBiWeeklyOn.IsVisible = false;
+            clicked = false;
+        }
+        else
+        {
+            pgMonthlyOn.IsVisible = false;
+            clicked = true;
+        }
+    }
 }
