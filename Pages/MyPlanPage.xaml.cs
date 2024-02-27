e94bef644e0fb504a18f6b86bdcfa3d005a9046c
Lis <lisleitora@gmail.com>
2024-02-27 16:27:51 +0000

    MyPlanPage selec. Extras cs and start CalendarPage

--- Pages/MyPlanPage.xaml.cs
+++ Pages/MyPlanPage.xaml.cs
@@ -85,4 +85,89 @@ public partial class MyPlanPage : ContentPage
             clicked = true;
         }
     }
+
+    private void Button_Clicked3(object sender, EventArgs e)
+    {
+        bool click = PgPink3.IsVisible;
+        if (click == true)
+        {
+            PgPink3.IsVisible = false;
+            click = false;
+        }
+        else
+        {
+            PgPink3.IsVisible = true;
+            click = true;
+        }
+    }
+    private void Button_Clicked4(object sender, EventArgs e)
+    {
+        bool click = PgPink4.IsVisible;
+        if (click == true)
+        {
+            PgPink4.IsVisible = false;
+            click = false;
+        }
+        else
+        {
+            PgPink4.IsVisible = true;
+            click = true;
+        }
+    }
+    private void Button_Clicked5(object sender, EventArgs e)
+    {
+        bool click = PgPink5.IsVisible;
+        if (click == true)
+        {
+            PgPink5.IsVisible = false;
+            click = false;
+        }
+        else
+        {
+            PgPink5.IsVisible = true;
+            click = true;
+        }
+    }
+    private void Button_Clicked6(object sender, EventArgs e)
+    {
+        bool click = PgPink6.IsVisible;
+        if (click == true)
+        {
+            PgPink6.IsVisible = false;
+            click = false;
+        }
+        else
+        {
+            PgPink6.IsVisible = true;
+            click = true;
+        }
+    }
+    private void Button_Clicked7(object sender, EventArgs e)
+    {
+        bool click = PgPink7.IsVisible;
+        if (click == true)
+        {
+            PgPink7.IsVisible = false;
+            click = false;
+        }
+        else
+        {
+            PgPink7.IsVisible = true;
+            click = true;
+        }
+    }
+    private void Button_Clicked8(object sender, EventArgs e)
+    {
+        bool click = PgPink8.IsVisible;
+        if (click == true)
+        {
+            PgPink8.IsVisible = false;
+            click = false;
+        }
+        else
+        {
+            PgPink8.IsVisible = true;
+            click = true;
+        }
+    }
 }
