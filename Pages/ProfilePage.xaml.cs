cfe0c5f55ad0c433eae3f622e27fc153c2db9040
Lis <lisleitora@gmail.com>
2024-04-19 19:11:27 +0100

    CalendarPage animation

--- Pages/ProfilePage.xaml.cs
+++ Pages/ProfilePage.xaml.cs
@@ -6,12 +6,21 @@ public partial class ProfilePage : ContentPage
 	{
 		InitializeComponent();
         
-	}
-
+    }
+    protected async override void OnAppearing()
+    {
+        base.OnAppearing();
+        await Task.Delay(200);
+        pgCalendar.TranslationX = pgCalendar.Width;
+        pgCalendar.TranslationY = pgCalendar.Height * -1;
+    }
     private async void GoCalendar(object sender, EventArgs e)
     {
     //    Application.Current.MainPage.Navigation.PushAsync(new CalendarPage());
     pgCalendarPage.IsVisible = true;
+        pgProfilePage.IsVisible = false;
+        await Helpers.AppearRight(pgCalendar, true);
+        await Helpers.BoingAppearUp(pgAssignments, true);
     }
 
     private void GoMyPlan(object sender, EventArgs e)
